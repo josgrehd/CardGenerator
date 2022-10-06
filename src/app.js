a9a60564904de6041bbf10f027bf0eb2086c477a
josgrehd <josgrehd@gmail.com>
2022-10-06 14:58:21 +0000

    Finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const Numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  const Palos = ["🔷", "♥️", "♠️", "♣️"];
+  const htmlNumero = document.querySelector("#numero");
+  const htmlPalo = document.querySelector("#palos");
+  const htmlPalo1 = document.querySelector("#palos1");
+
+  let ArrayAleatorio = array => {
+    let numeroAleatorio = Math.floor(Math.random() * array.length);
+    let resultado = array[numeroAleatorio];
+    return resultado;
+  };
+
+  let numeroPalos = ArrayAleatorio(Palos);
+  htmlNumero.innerHTML = ArrayAleatorio(Numeros);
+  htmlPalo.innerHTML = numeroPalos;
+  htmlPalo1.innerHTML = numeroPalos;
 };
